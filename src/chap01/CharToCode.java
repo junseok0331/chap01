702b1da84b20438beb902ef91dac815ee74f45fe
junseok125 <129023120+junseok125@users.noreply.github.com>
2023-03-27 13:36:14 +0900

    Update CharToCode.java

--- src/chap01/CharToCode.java
+++ src/chap01/CharToCode.java
@@ -9,6 +9,7 @@ public class CharToCode {
 			char characteC = 'a';
 			int charCode = (int)characteC;
 			System.out.printf("%c = %d", characteC,charCode);
+		System.out.printf("%c = %d", characteC,charCode);
 		
 	}
 
